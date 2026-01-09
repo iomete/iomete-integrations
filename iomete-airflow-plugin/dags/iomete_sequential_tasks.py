c4336c5abfc922ffa4222fb76eec34f2ab4d3fe7
rocco.verhoef <rcjverhoef@gmail.com>
2026-01-09 15:20:11 +0900

    remove blue, just use black to solve security vulnerability in blue

--- iomete-airflow-plugin/dags/iomete_sequential_tasks.py
+++ iomete-airflow-plugin/dags/iomete_sequential_tasks.py
@@ -3,26 +3,26 @@ from airflow import DAG, utils
 from iomete_airflow_plugin.iomete_operator import IometeOperator
 
 args = {
-    'owner': 'airflow',
-    'email': ['airflow@example.com'],
-    'depends_on_past': False,
-    'start_date': utils.dates.days_ago(0, second=1),
+    "owner": "airflow",
+    "email": ["airflow@example.com"],
+    "depends_on_past": False,
+    "start_date": utils.dates.days_ago(0, second=1),
 }
 
-dag = DAG(dag_id='iomete-demo', default_args=args, schedule_interval=None)
+dag = DAG(dag_id="iomete-demo", default_args=args, schedule_interval=None)
 
 catalogTask = IometeOperator(
-    task_id='task-01-sync-catalog',
-    job_id='iomete-catalog-sync',
+    task_id="task-01-sync-catalog",
+    job_id="iomete-catalog-sync",
     dag=dag,
-    variable_prefix='iomete_',
+    variable_prefix="iomete_",
 )
 
 sqlTask = IometeOperator(
-    task_id='task-02-run-sql',
-    job_id='sql-runner',
+    task_id="task-02-run-sql",
+    job_id="sql-runner",
     dag=dag,
-    variable_prefix='iomete_',
+    variable_prefix="iomete_",
 )
 
 sqlTask >> catalogTask
--- iomete-airflow-plugin/iomete_airflow_plugin/iomete_operator.py
+++ iomete-airflow-plugin/iomete_airflow_plugin/iomete_operator.py
@@ -8,8 +8,8 @@ from airflow.models import BaseOperator
 
 from iomete_airflow_plugin.hook import IometeHook
 
-XCOM_RUN_ID_KEY = 'job_run_id'
-XCOM_JOB_ID_KEY = 'job_id'
+XCOM_RUN_ID_KEY = "job_run_id"
+XCOM_JOB_ID_KEY = "job_id"
 
 
 class IometeOperator(BaseOperator):
@@ -18,12 +18,12 @@ class IometeOperator(BaseOperator):
     """
 
     # Used in airflow.models.BaseOperator
-    template_fields = ('job_id', 'config_override')
-    template_ext = ('.json',)
+    template_fields = ("job_id", "config_override")
+    template_ext = (".json",)
 
     # IOMETE blue color with white text
-    ui_color = '#fff'
-    ui_fgcolor = '#0070f3'
+    ui_color = "#fff"
+    ui_fgcolor = "#0070f3"
 
     def __init__(
         self,
@@ -31,7 +31,7 @@ class IometeOperator(BaseOperator):
         config_override: Optional[Union[Dict, str]] = None,
         polling_period_seconds: int = 10,
         do_xcom_push: bool = False,
-        variable_prefix: str = 'iomete_',
+        variable_prefix: str = "iomete_",
         **kwargs,
     ):
         """
@@ -42,7 +42,7 @@ class IometeOperator(BaseOperator):
         self.do_xcom_push = do_xcom_push
         self.payload = {}
 
-        self.run_id = ''
+        self.run_id = ""
         self.job_id = job_id
         self.polling_period_seconds = polling_period_seconds
 
@@ -55,18 +55,18 @@ class IometeOperator(BaseOperator):
 
         if self.job_id is None:
             raise AirflowException(
-                'Parameter `job_id` should be specified. '
-                'You can also specify the name of the IOMETE job in `job_id` field.'
+                "Parameter `job_id` should be specified. "
+                "You can also specify the name of the IOMETE job in `job_id` field."
             )
 
     def execute(self, context):
-        self.log.info('Submitting IOMETE Job')
+        self.log.info("Submitting IOMETE Job")
         hook = IometeHook(
             variable_prefix=self.variable_prefix,
         )
         dict_data = serialize_to_dict(self.config_override)
-        self.run_id = hook.submit_job_run(self.job_id, dict_data)['id']
-        self.log.info(f'IOMETE Job submitted. Run ID {self.run_id}')
+        self.run_id = hook.submit_job_run(self.job_id, dict_data)["id"]
+        self.log.info(f"IOMETE Job submitted. Run ID {self.run_id}")
         self._monitor_app(hook, context)
 
     def on_kill(self):
@@ -75,7 +75,7 @@ class IometeOperator(BaseOperator):
         )
         hook.cancel_job_run(self.job_id, self.run_id)
         self.log.info(
-            'Task: %s with job id: %s was requested to be cancelled.',
+            "Task: %s with job id: %s was requested to be cancelled.",
             self.task_id,
             self.job_id,
         )
@@ -83,24 +83,24 @@ class IometeOperator(BaseOperator):
     def _monitor_app(self, hook, context):
 
         if self.do_xcom_push:
-            context['ti'].xcom_push(key=XCOM_JOB_ID_KEY, value=self.job_id)
-        self.log.info('Spark job submitted with job_id: %s', self.job_id)
+            context["ti"].xcom_push(key=XCOM_JOB_ID_KEY, value=self.job_id)
+        self.log.info("Spark job submitted with job_id: %s", self.job_id)
         if self.do_xcom_push:
-            context['ti'].xcom_push(key=XCOM_RUN_ID_KEY, value=self.run_id)
+            context["ti"].xcom_push(key=XCOM_RUN_ID_KEY, value=self.run_id)
 
         while True:
             app = hook.get_job_run(self.job_id, self.run_id)
             app_state = _get_state_from_app(app)
             if app_state.is_final:
                 if app_state.is_successful:
-                    self.log.info('%s completed successfully.', self.task_id)
+                    self.log.info("%s completed successfully.", self.task_id)
                     return
                 else:
-                    error_message = '{t} failed with terminal state: {s}'.format(t=self.task_id, s=app_state.value)
+                    error_message = "{t} failed with terminal state: {s}".format(t=self.task_id, s=app_state.value)
                     raise AirflowException(error_message)
             else:
-                self.log.info('%s in app state: %s', self.task_id, app_state.value)
-                self.log.info('Sleeping for %s seconds.', self.polling_period_seconds)
+                self.log.info("%s in app state: %s", self.task_id, app_state.value)
+                self.log.info("Sleeping for %s seconds.", self.polling_period_seconds)
                 time.sleep(self.polling_period_seconds)
 
 
@@ -122,24 +122,24 @@ def serialize_to_dict(config_override: Optional[Union[Dict, str]] = None) -> Dic
         # Convert string representation of dict to actual dict
         return eval(config_override)
     except:
-        raise ValueError(f'Unsupported format for config_override: {config_override}')
+        raise ValueError(f"Unsupported format for config_override: {config_override}")
 
 
 def _get_state_from_app(app):
-    return ApplicationStateType(app.get('driverStatus', ''))
+    return ApplicationStateType(app.get("driverStatus", ""))
 
 
 class ApplicationStateType(Enum):
-    EmptyState = 'ENQUEUED'
-    DeployingState = 'SUBMITTED'  # Usually takes ~1 min
-    RunningState = 'RUNNING'
-    CompletedState = 'COMPLETED'
-    FailedState = 'FAILED'
-    AbortedState = 'ABORTED'
-    AbortingState = 'ABORTING'
+    EmptyState = "ENQUEUED"
+    DeployingState = "SUBMITTED"  # Usually takes ~1 min
+    RunningState = "RUNNING"
+    CompletedState = "COMPLETED"
+    FailedState = "FAILED"
+    AbortedState = "ABORTED"
+    AbortingState = "ABORTING"
     ExecutorState = map(
-        {'RUNNING': 1},
-        {'PENDING': 1},  # Take ~1 min to scale executor
+        {"RUNNING": 1},
+        {"PENDING": 1},  # Take ~1 min to scale executor
     )
 
     @property
