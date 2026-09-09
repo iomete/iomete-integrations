aaad3196036197c439bdeef7091753c17cf91193
Ujjawal Khare <138279087+ujjawal-khare-27@users.noreply.github.com>
2026-09-09 14:31:50 +0530

    Revert "Adding airflow 2 support for iomete airflow plugin" (#77)
    Reverts iomete/iomete-integrations#53

--- iomete-airflow-plugin/iomete_airflow_plugin/plugin.py
+++ iomete-airflow-plugin/iomete_airflow_plugin/plugin.py
@@ -1,26 +1,18 @@
 from airflow.plugins_manager import AirflowPlugin
+from flask import Blueprint
 
 from iomete_airflow_plugin.hook import IometeHook
 from iomete_airflow_plugin.iomete_operator import IometeOperator
 
 plugin_name = "iomete"
 
-# Flask ships with the Airflow 2.x webserver but was dropped in Airflow 3 (FastAPI-based UI).
-# Register the blueprint only when Flask is importable so the plugin loads on both.
-try:
-    from flask import Blueprint
-
-    _flask_blueprints = [
-        Blueprint(
-            plugin_name,
-            __name__,
-            template_folder="templates",  # registers airflow/plugins/templates as a Jinja template folder
-            static_folder="static",
-            static_url_path="/static/" + plugin_name,
-        )
-    ]
-except ImportError:
-    _flask_blueprints = []
+bp = Blueprint(
+    plugin_name,
+    __name__,
+    template_folder="templates",  # registers airflow/plugins/templates as a Jinja template folder
+    static_folder="static",
+    static_url_path="/static/" + plugin_name,
+)
 
 
 class IometePlugin(AirflowPlugin):
@@ -30,5 +22,5 @@ class IometePlugin(AirflowPlugin):
     executors = []
     macros = []
     admin_views = []
-    flask_blueprints = _flask_blueprints
+    flask_blueprints = [bp]
     menu_links = []
